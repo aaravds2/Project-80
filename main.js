5cb3e1511812175f4893884796f71852ea9c1be7
aaravds2 <83860100+aaravds2@users.noreply.github.com>
2021-05-22 15:14:15 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+var name_of_the_guest_in_array = []
+function submit() {
+
+var name_of_the_guest = document.getElementById("name_guest").value;
+
+name_of_the_guest_in_array.push(name_of_the_guest);
+console.log(name_of_the_guest_in_array);
+
+for
+
+}
